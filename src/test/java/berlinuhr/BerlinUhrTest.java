698af169ae148af3f8ca35de234d6b59655dcf0a
Ashley Hindmarsh <ash+github@best-scarper.co.uk>
2018-09-13 11:24:04 +0100

    Groups tests and name assertions - less wordy, and function-oriented

--- src/test/java/berlinuhr/BerlinUhrTest.java
+++ src/test/java/berlinuhr/BerlinUhrTest.java
@@ -1,5 +1,6 @@
 package berlinuhr;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -12,8 +13,17 @@ public class BerlinUhrTest {
     }
 
     @Test
-    public void oneSecondsPastMidnight() {
-        assertArrayEquals(BerlinUhr.fromDigital("00:00:01"), new int[]{0,0,0,0,0});
+    public void seconds() {
+        assertArrayEquals("one second", BerlinUhr.fromDigital(  "00:00:01"), new int[]{1,0,0,0,0});
+        assertArrayEquals("two seconds", BerlinUhr.fromDigital(  "00:00:02"), new int[]{0,0,0,0,0});
+        assertArrayEquals("seconds edge case", BerlinUhr.fromDigital("00:00:58"), new int[]{1,0,0,0,0});
+        assertArrayEquals("seconds edge case", BerlinUhr.fromDigital("00:00:59"), new int[]{0,0,0,0,0});
+    }
+
+    @Ignore
+    @Test
+    public void acceptance() {
+        assertArrayEquals(BerlinUhr.fromDigital("10:31:00"), new int[]{1,2,0,6,1});
     }
 
 
